29997e1d5decf1933f8604d0d2295951b3079846
Anton Khanaev <askhanaev@edu.hse.ru>
2020-12-23 22:06:55 +0300

    Insertions to supply table added.

---
+++  1. Основы реляционной модели и SQL/5. Запросы корректировки данных/1.5.3-insert_select.sql
@@ -0,0 +1,4 @@
+INSERT INTO book(title, author, price, amount)
+          SELECT title, author, price, amount
+FROM supply WHERE author NOT IN ('Булгаков М.А.', 'Достоевский Ф.М.');
+SELECT * FROM book;
